907903a9217c4d9788e6747dd27f1902545f4db7
Ruka Sakurai <ruka.sakurai@gmail.com>
2013-04-05 22:04:10 +0900

    adding other areas

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php include_once("home.html"); ?>
+<html>
+<?php include_once("head.html"); ?>
+<body>
+<?php include_once("top_part.html"); ?>
+<div id="widewrapper"><div id="contentsArea"><div id="mainArea">
+<?php 
+if(isset($_GET["area"])){
+  include_once($_GET["area"].".html");
+} else {
+  include_once("metropolitanarea.html");
+}
+?>
+</div></div></div>
+</body>
+</html>
